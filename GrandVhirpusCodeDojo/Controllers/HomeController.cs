a936689bbaf86c786ecad1b98d7baba77e542fd0
RumiChakravarty <rumi.sarma.c@gmail.com>
2016-09-29 14:53:35 -0400

    Lab 21

---
+++ GrandVhirpusCodeDojo/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using GrandVhirpusCodeDojo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GrandVhirpusCodeDojo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RegistrationForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RegistrationForm(Registration registration)
+        {
+            if (ModelState.IsValid) { 
+            ViewBag.DisplayDate = DateTime.Now.AddDays(20).ToString("dd/mm/yyyy");
+                          return View("ThankYou", registration);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ GrandVhirpusCodeDojo/Models/Registration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GrandVhirpusCodeDojo.Models
+{
+    public class Registration
+    {
+        [Required(ErrorMessage = "Please enter your First name")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Please enter your Last name")]
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        [Required(ErrorMessage = "Please enter your email address")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Please select Country")]
+        public string Country { get; set; }
+        [Required(ErrorMessage = "Please enter your Zipcode")]
+        public string Zipcode { get; set; }
+        [Required(ErrorMessage = "Please select course option")]
+        public string Course { get; set; }
+    }
+}
